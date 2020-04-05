c067bda55ffecbdec3478d4a6881f6c26f2cc2ac
nathan denholm <nathandenholm@live.co.uk>
2020-04-05 14:23:43 +0100

    what

--- game/game_objects.py
+++ game/game_objects.py
@@ -1,11 +1,12 @@
 import pygame
-
+import random
+#need to somehow find out jhow a player will know where they afre on the board.. 
 class Square:
     """
     Base class
     All squares on monopoly board will inherit from this.
     """
-    def __init__(self, x, y, w, h, square_id):
+    def __init__(self, x, y, w, h, square_id, _property):
 
         self.x = x
         self.y = y
@@ -14,6 +15,8 @@ class Square:
         self.id = square_id
         self.hover = False
         self.is_clicked = False
+        self.property = _property #when creating the square objecs, in order to handle chaneg and communiuty chest. just pass _property as some sort of stryucture
+
 
     def check_if_hover(self):
 
@@ -23,27 +26,69 @@ class Square:
             
         else:
             self.hover = False
-            
-            
-    def is_clicked():
-        # i think this shoud be doone in the event loop / find ut later..
-        # will need to create a funcetio that loops through the squuares and checks if they are clicked.
+
+class player:
+    def __init__(self, piece):
+
+        self.properties_owned = []
+        self.get_out_of_jail_free = 0
+        self.piece = piece
+        self.wallet = 1500
+        self.turn_is_over = True
+        self.bankrupt = False
+        self.in_jail = False
+        self.doubles_rolled = 0 # use to check if they go to jail
+
+    def check_if_bankrupt(self):
+        pass
+
+    def check_if_has_set(self):
+        pass
+
+    def roll_and_move(self):
+
+        pass
+
+    def mortage_property(self):
+        pass
+
+    def buy_property(self):
+        pass
+
+    def send_to_jail(self):
         pass
 
 
-class MonopolyProperty(Square):
+class Property:
     """
-    square_id: unique id for that square
-    rent_x: cost of rent with x houses
-   
+    Base class for monopoly properties
     """
-    
-    def __init__(self, x, y, w, h, square_id, property_name, property_value, rent, rent_1, rent_2, rent_3, rent_4, rent_with_hotel):
-        super().__init__(x, y, w, h, square_id)
+    def __init__(self, name, value):
+
+        self.name = name 
+        self.value = value
+        self.mortage_value = self.value/2
+        self.is_owned = False
+        self.property_owner = None
+
+class UtilityProperty(Property):
+    def __init__(self, type):
+        super().__init__(name, value)
+
+    pass
+
+class StationProperty(Property):
+    def __init__(self):
+        super().__init__(name,value)
+        pass
         
-        self.property_name = property_name
-        self.property_value = property_value
-        self.mortage_value = self.property_value/2
+class MonopolyProperty(Property):
+    def __init__(self, colour, rent, rent_1, rent_2, rent_3, rent_4):
+        super().__init__(name, value)
+        
+        self.colour = colour
+        self.number_of_houses = 0
+        self.has_hotel = False
         self.rent = rent 
         self.rent_with_set = self.rent*2
         self.rent_1 = rent_1
@@ -51,13 +96,27 @@ class MonopolyProperty(Square):
         self.rent_3 = rent_3 
         self.rent_4 = rent_4 
         self.rent_with_hotel = rent_with_hotel
-      
-       
 
-    def test(self):
-        #print(self.rent_data[self.property_name]["rent_with_set"])
-        if self.hover:
-            print(self.property_name)
+
+
+
+def roll_dice(player):
+    number_1 = random.randint(1,6)
+    number_2 = random.randint(1,6)
+    if number_1 == number_2:
+        player.doubles_rolled += 1
+    return number_1 +number_2
+
+    
+
+
+
+
+
+
+
+
+
 
 
 
--- game/monopoly.py
+++ game/monopoly.py
@@ -4,17 +4,14 @@ from game_objects import MonopolyProperty
 pygame.init()
 window = pygame.display.set_mode((1000,1000))
 board_img = pygame.image.load(os.path.join("assets","board2.jpg"))
-test = MonopolyProperty(3,787,130,80,2,"roverway",100)
-test2=  MonopolyProperty(3,622,130,80,2,"merciard",100)
+# test = MonopolyProperty(3,787,130,80,2,"roverway",100)
+# test2=  MonopolyProperty(3,622,130,80,2,"merciard",100)
 #cord = pygame.mouse.get_pos()
 
 def main_game_loop():
     while True:
        
-        test.test()
-        test.check_if_hover()
-        test2.test()
-        test2.check_if_hover()
+       
         #cord = pygame.mouse.get_pos()
         #print(cord)
         
